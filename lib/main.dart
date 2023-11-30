bb220dec370a0d4324ea85c73ceabda487ce3d48
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-11-30 17:03:27 +0100

    update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -10,6 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,

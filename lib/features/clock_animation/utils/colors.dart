79a2813055128d9adfb3ea7eaff65cc239d36e15
nikkieke <95222620+nikkieke@users.noreply.github.com>
2024-01-30 14:37:02 +0100

    add clock screens colors

---
+++ lib/features/clock_animation/utils/colors.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class UiColors{
+  static const Color mustard = Color(0xfffccd6c);
+  static const Color darkViolet = Color(0xff432243);
+  static const Color cherryRed = Color(0xfffe4b4c);
+  static const Color darkMagenta = Color(0xff422343);
+  static const Color coolViolet = Color(0xff7a647b);
+  static const Color lightBlueBG = Color(0xffeaf0fa);
+  static const Color lightBlueAccent = Color(0xffeaf0f8);
+  static const Color lightBlueCard = Color(0xffe6eefa);
+}

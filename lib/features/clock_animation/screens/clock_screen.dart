def07c26470f44423e976898b7f7eb1970b93c35
nikkieke <nikkieke001@gmail.com>
2024-02-28 20:14:39 +0100

    refactor

--- lib/features/clock_animation/screens/clock_screen.dart
+++ lib/features/clock_animation/screens/clock_screen.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_micro_interactions/features/features.dart';
+
 
 class ClockScreen extends StatelessWidget {
   const ClockScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return const Scaffold(
+      backgroundColor: UiColors.lightBlueBG,
+    );
   }
 }

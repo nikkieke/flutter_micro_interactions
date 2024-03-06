9f6e12162d12a76a70ed17b1d1bd3616ed632b5b
nikkieke <nikkieke001@gmail.com>
2024-03-06 18:27:16 +0100

    add text widget

---
+++ lib/features/clock_animation/utils/text.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_micro_interactions/features/clock_animation/clock_animation.dart';
+
+
+class UiTexts extends StatelessWidget {
+  const UiTexts({
+    super.key, required this.text, required this.size,
+  });
+
+  final String text;
+  final double size;
+
+
+  @override
+  Widget build(BuildContext context) {
+    return
+    Text(
+      text,
+      style: TextStyle(
+      color: UiColors.darkMagenta,
+      fontWeight: FontWeight.bold,
+      fontSize: size,
+    ),
+    );
+  }
+}

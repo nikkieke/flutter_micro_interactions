f0a5a81fb7f6fea4fa1581c38df57f766bce393e
nikkieke <nikkieke001@gmail.com>
2024-03-29 21:43:34 +0100

    update path

--- lib/features/clock_animation/widget/widget.dart
+++ lib/features/clock_animation/widget/widget.dart
@@ -2,3 +2,7 @@ export 'button.dart';
 export 'buttom_nav_widget.dart';
 export 'alarm_widget.dart';
 export 'clock_face.dart';
+export 'second_hand.dart';
+export 'minute_hand.dart';
+export 'hour_hand.dart';
+export 'clock_dial_painter.dart';
